a4ace2abca820626175ef43b31a8334f5fc2be0a
professor6777 <billionaireprofessor87@gmail.com>
2025-11-04 21:23:23 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~csOAnE", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "923413941122", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY AB CHANDIO ( MR AB MODS OFC 🇿🇼 )
